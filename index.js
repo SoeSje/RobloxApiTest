ea4106cc40f7fef0332488bcdda24087c706873b
SoeSje <41758893+SoeSje@users.noreply.github.com>
2020-07-27 01:52:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const https = require('https');
+
+
+const app = express();
+
+app.use(express.static("public"));
+
+
+
+app.get("/", (req, res) => {
+
+
+
+
+    https.get('https://catalog.roblox.com/v1/favorites/assets/2263341598/count', (res) => {
+        console.log('statusCode:', res.statusCode);
+      
+        res.on('data', (d) => {
+          process.stdout.write(d);
+          res.send(d);
+        });
+
+      
+      }).on('error', (e) => {
+        console.error(e);
+      });
+
+});
+
+
+
+
+
